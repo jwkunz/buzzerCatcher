6ee90068976bdda7d46d0c575b3c3133a69b1bbf
Jakob Kunzler <jakob.w.kunzler.civ@us.navy.mil>
2025-03-26 20:28:18 -0400

    Added songs by boys

--- code/game/song_loading.h
+++ code/game/song_loading.h
@@ -99,3 +99,107 @@ static const uint8_t song_loading[SONG_LOADING_LENGTH]{
     16
     // END OF SONG
 };
+
+/*
+
+// This is "Star Wars"
+#define SONG_LOADING_LENGTH (2 * 27 + 1)
+static const uint8_t song_loading[SONG_LOADING_LENGTH]{
+    // Length is 2X number of notes + 1
+
+    // Preamble
+
+    120, // Beats per minute
+
+    // Insert pairs of key number and duration
+
+    // First value in pair is the piano key (use the piano_key_definitions #defines)
+    // Second value in pair is the number beats in terms of 1/32 notes
+    // e.g. 1/32 note = 1, 1/8 note = 4, 1/4 note = 8
+    // Reccomend adding // to separate notes and using // MEASURE markings to stay organized!
+
+    //
+    KEY_D_4,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_D_4,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_D_4,
+    2,
+    // 
+    KEY_G_4,
+    16,
+    //
+    KEY_D_5,
+    16,
+    //
+    KEY_C_5,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_B_6,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_A_6,
+    1,
+    //
+    KEY_G_5,
+    16,
+    //
+    KEY_D_5,
+    8,
+    //
+    KEY_C_5,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_D_5,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_A_6,
+    2,
+    //
+    KEY_G_5,
+    16,
+    //
+    KEY_D_5,
+    8,
+    //
+    KEY_C_5,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_B_6,
+    2,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_C_5,
+    2,
+    //
+    KEY_A_4,
+    16
+        // END OF SONG
+};
+
+*/
--- code/game/song_response.h
+++ code/game/song_response.h
@@ -104,3 +104,61 @@ static const uint8_t song_response[SONG_RESPONSE_LENGTH]{
     // END OF SONG
 };
 */
+
+/*
+// This is something made up by Canyon
+#define SONG_RESPONSE_LENGTH (2*12+1)
+static const uint8_t song_response[SONG_LOADING_LENGTH]{
+    // Length is 2X number of notes + 1
+
+    // Preamble
+
+    120, // Beats per minute
+
+    // Insert pairs of key number and duration
+
+    // First value in pair is the piano key (use the piano_key_definitions #defines)
+    // Second value in pair is the number beats in terms of 1/32 notes
+    // e.g. 1/32 note = 1, 1/8 note = 4, 1/4 note = 8
+    // Reccomend adding // to separate notes and using // MEASURE markings to stay organized!
+
+    // MEASURE 1
+    KEY_B_5,
+    15,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_A_6,
+    15,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_B_6,
+    15,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_C_6,
+    15,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_B_6,
+    15,
+    //
+    KEY_REST,
+    1,
+    //
+    KEY_B_6,
+    15
+    //
+    KEY_REST,
+    1,
+
+    // END OF SONG
+};
+*/
